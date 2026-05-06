63a8fad30ee24818971e3d49eea243176a625188
junhyeongchoii <k14414@naver.com>
2026-05-07 01:05:09 +0900

    fix : moolog -> Emolog

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:moolog/main.dart';
+import 'package:Emolog/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
